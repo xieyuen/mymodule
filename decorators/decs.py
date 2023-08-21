06da58f0a5698a9ee5957aad261091dcbf839eb6
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-08-21 17:50:20 +0800

    Update decs.py

--- decorators/decs.py
+++ decorators/decs.py
@@ -6,7 +6,6 @@ from .. import logger
 
 def elapsed_time(func):
     def wrapper(*args, **kwargs):
-        import time
         start = time.time()
         func(*args, **kwargs)
         end = time.time()
