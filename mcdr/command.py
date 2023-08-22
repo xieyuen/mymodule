1578fed73628518adeeb44bac1d42e993a3f10c6
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-08-23 01:56:48 +0800

    添加 `__all__`，然后格式化

--- mcdr/command.py
+++ mcdr/command.py
@@ -1,29 +1,28 @@
-'''
+"""
 MCDR 指令编辑包
 `from mymodule.mcdr.command import *`就行了
-'''
-
+"""
 
 # Command Arguments
 from mcdreforged.command.builder.nodes.arguments import \
-	Number, Integer, Float, Text, QuotableText, \
-	GreedyText, Boolean, Enumeration
-from mcdreforged.command.builder.nodes.basic import AbstractNode, Literal, ArgumentNode
+    Number, Integer, Float, Text, QuotableText, \
+    GreedyText, Boolean, Enumeration
+from mcdreforged.command.builder.nodes.basic import \
+    AbstractNode, Literal, ArgumentNode
 
-# PLugin Server Interface
+# Plugin Server Interface
 from mcdreforged.api.types import ServerInterface, PluginServerInterface
 
 # Simple Command Builder
 from mcdreforged.api.command import SimpleCommandBuilder
-SCB = builder = SimpleCommandBuilder()
 
 # Exceptions
 from mcdreforged.command.builder.exception import \
-	LiteralNotMatch, NumberOutOfRange, IllegalArgument, EmptyText, \
-	UnknownCommand, UnknownArgument, CommandSyntaxError, UnknownRootArgument, \
-	RequirementNotMet, IllegalNodeOperation, CommandError, InvalidNumber, \
-	InvalidInteger, InvalidFloat, UnclosedQuotedString, IllegalEscapesUsage, InvalidBoolean, \
-	InvalidEnumeration, TextLengthOutOfRange, CommandErrorBase, AbstractOutOfRange
+    LiteralNotMatch, NumberOutOfRange, IllegalArgument, EmptyText, \
+    UnknownCommand, UnknownArgument, CommandSyntaxError, UnknownRootArgument, \
+    RequirementNotMet, IllegalNodeOperation, CommandError, InvalidNumber, \
+    InvalidInteger, InvalidFloat, UnclosedQuotedString, IllegalEscapesUsage, InvalidBoolean, \
+    InvalidEnumeration, TextLengthOutOfRange, CommandErrorBase, AbstractOutOfRange
 
 # Plugin things
 from mcdreforged.plugin.meta.metadata import Metadata
@@ -38,8 +37,86 @@ from mcdreforged.permission.permission_level import PermissionLevel
 
 # Command sources
 from mcdreforged.command.command_source import \
-	CommandSource, ConsoleCommandSource, PlayerCommandSource, \
-	InfoCommandSource, PluginCommandSource
+    CommandSource, ConsoleCommandSource, PlayerCommandSource, \
+    InfoCommandSource, PluginCommandSource
 
 # Loggers
 from mcdreforged.utils.logger import SyncStdoutStreamHandler, MCDReforgedLogger
+
+
+SCB = builder = SimpleCommandBuilder()
+
+
+__all__ = [
+    # -------------------------
+    #     Command Arguments
+    # -------------------------
+
+    'Number', 'Integer', 'Float',
+    'Text', 'Enumeration',
+    'QuotableText', 'AbstractNode',
+    'GreedyText', 'Literal',
+    'Boolean', 'ArgumentNode',
+
+    # -------------------------
+    #     Server Interface
+    # -------------------------
+
+    'ServerInterface', 'PluginServerInterface',
+
+    # -------------------------
+    #     Command Builder
+    # -------------------------
+
+    'SimpleCommandBuilder', 'SCB', 'builder',
+
+    # --------------------------
+    #   MCDReforged Exceptions
+    # --------------------------
+
+    'LiteralNotMatch', 'UnclosedQuotedString',
+    'NumberOutOfRange', 'IllegalEscapesUsage',
+    'IllegalArgument', 'InvalidBoolean',
+    'EmptyText', 'InvalidEnumeration',
+    'UnknownCommand', 'TextLengthOutOfRange',
+    'UnknownArgument', 'CommandErrorBase',
+    'CommandSyntaxError', 'AbstractOutOfRange',
+    'UnknownRootArgument', 'InvalidNumber',
+    'RequirementNotMet', 'InvalidInteger',
+    'IllegalNodeOperation', 'InvalidFloat',
+    'CommandError',
+
+    # -------------------------
+    #       Plugin Things
+    # -------------------------
+
+    'Metadata',
+    'Version',
+    'VersionRequirement',
+
+    # -------------------------
+    #        Server Info
+    # -------------------------
+
+    'Info', 'ServerInformation',
+
+    # -------------------------
+    #     Permission Level
+    # -------------------------
+
+    'PermissionLevel',
+
+    # ------------------------
+    #      Command Source
+    # ------------------------
+
+    'CommandSource', 'InfoCommandSource',
+    'ConsoleCommandSource', 'PluginCommandSource',
+    'PlayerCommandSource',
+
+    # ------------------------
+    #    MCDReforged Logger
+    # ------------------------
+    'SyncStdoutStreamHandler',
+    'MCDReforgedLogger',
+]
