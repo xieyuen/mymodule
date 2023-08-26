cb3fc9309837353ba25dc9083bf278434c558221
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-08-26 13:29:42 +0800

    新增`sha3`算法、`blake`算法和`shake`算法

--- utils/hash.py
+++ utils/hash.py
@@ -1,26 +1,26 @@
 import hashlib
 
 
-def sha256(data: str | bytes, *, encode='utf-8', decode='gbk'):
-    if isinstance(data, bytes):
-        return \
-            hashlib.sha256(
-                data.decode(decode).encode(encode)
-            ).hexdigest()
-    
-    return \
-        hashlib.sha256(data.encode(encode)).hexdigest()
-
-
 def md5(data: str | bytes, *, encode='utf-8', decode='gbk'):
     if isinstance(data, bytes):
         return \
             hashlib.md5(
                 data.decode(decode).encode(encode)
             ).hexdigest()
-    
+
     return \
         hashlib.md5(data.encode(encode)).hexdigest()
+
+
+def sha256(data: str | bytes, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.sha256(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+
+    return \
+        hashlib.sha256(data.encode(encode)).hexdigest()
 
 
 def sha512(data: str | bytes, *, encode='utf-8', decode='gbk'):
@@ -29,7 +29,7 @@ def sha512(data: str | bytes, *, encode='utf-8', decode='gbk'):
             hashlib.sha512(
                 data.decode(decode).encode(encode)
             ).hexdigest()
-    
+
     return \
         hashlib.sha512(data.encode(encode)).hexdigest()
 
@@ -40,7 +40,7 @@ def sha384(data: str | bytes, *, encode='utf-8', decode='gbk'):
             hashlib.sha384(
                 data.decode(decode).encode(encode)
             ).hexdigest()
-    
+
     return \
         hashlib.sha384(data.encode(encode)).hexdigest()
 
@@ -51,7 +51,7 @@ def sha1(data: str | bytes, *, encode='utf-8', decode='gbk'):
             hashlib.sha1(
                 data.decode(decode).encode(encode)
             ).hexdigest()
-    
+
     return \
         hashlib.sha1(data.encode(encode)).hexdigest()
 
@@ -62,6 +62,94 @@ def sha224(data: str | bytes, *, encode='utf-8', decode='gbk'):
             hashlib.sha224(
                 data.decode(decode).encode(encode)
             ).hexdigest()
-    
+
     return \
         hashlib.sha224(data.encode(encode)).hexdigest()
+
+
+def sha3_224(data: str | bytes, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.sha3_224(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+
+    return \
+        hashlib.sha3_224(data.encode(encode)).hexdigest()
+
+
+def sha3_256(data: str | bytes, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.sha3_256(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+
+    return \
+        hashlib.sha3_256(data.encode(encode)).hexdigest()
+
+
+def sha3_384(data: str | bytes, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.sha3_384(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+
+    return \
+        hashlib.sha3_384(data.encode(encode)).hexdigest()
+
+
+def sha3_512(data: str | bytes, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.sha3_512(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+
+    return \
+        hashlib.sha3_512(data.encode(encode)).hexdigest()
+
+
+def blake2b(data: str | bytes, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.blake2b(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+
+    return \
+        hashlib.blake2b(data.encode(encode)).hexdigest()
+
+
+def blake2s(data: str | bytes, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.blake2s(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+
+    return \
+        hashlib.blake2s(data.encode(encode)).hexdigest()
+
+
+def shake128(data: str | bytes, length: int, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.shake_128(
+                data.decode(decode).encode(encode)
+            ).hexdigest(length)
+
+    return \
+        hashlib.shake_128(data.encode(encode)).hexdigest(length)
+
+
+def shake256(data: str | bytes, length, *, encode='utf-8', decode='gbk'):
+    if isinstance(data, bytes):
+        return \
+            hashlib.shake_256(
+                data.decode(decode).encode(encode)
+            ).hexdigest(length)
+
+    return \
+        hashlib.shake_256(data.encode(encode)).hexdigest(length)
