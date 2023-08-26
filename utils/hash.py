c38b3efab0cd6522857537d4e5b6a6f977d3b97e
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-08-26 11:16:25 +0800

    新增`decode`参数(仅`bytes`)

--- utils/hash.py
+++ utils/hash.py
@@ -1,37 +1,67 @@
 import hashlib
 
 
-def sha256(data: str | bytes, *, encode='utf-8'):
+def sha256(data: str | bytes, *, encode='utf-8', decode='gbk'):
     if isinstance(data, bytes):
-        return hashlib.sha256(data).hexdigest()
-    return hashlib.sha256(data.encode(encode)).hexdigest()
+        return \
+            hashlib.sha256(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+    
+    return \
+        hashlib.sha256(data.encode(encode)).hexdigest()
 
 
-def md5(data: str | bytes, *, encode='utf-8'):
+def md5(data: str | bytes, *, encode='utf-8', decode='gbk'):
     if isinstance(data, bytes):
-        return hashlib.md5(data).hexdigest()
-    return hashlib.md5(data.encode(encode)).hexdigest()
+        return \
+            hashlib.md5(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+    
+    return \
+        hashlib.md5(data.encode(encode)).hexdigest()
 
 
-def sha512(data: str | bytes, *, encode='utf-8'):
+def sha512(data: str | bytes, *, encode='utf-8', decode='gbk'):
     if isinstance(data, bytes):
-        return hashlib.sha512(data).hexdigest()
-    return hashlib.sha512(data.encode(encode)).hexdigest()
+        return \
+            hashlib.sha512(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+    
+    return \
+        hashlib.sha512(data.encode(encode)).hexdigest()
 
 
-def sha384(data: str | bytes, *, encode='utf-8'):
+def sha384(data: str | bytes, *, encode='utf-8', decode='gbk'):
     if isinstance(data, bytes):
-        return hashlib.sha384(data).hexdigest()
-    return hashlib.sha384(data.encode(encode)).hexdigest()
+        return \
+            hashlib.sha384(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+    
+    return \
+        hashlib.sha384(data.encode(encode)).hexdigest()
 
 
-def sha1(data: str | bytes, *, encode='utf-8'):
+def sha1(data: str | bytes, *, encode='utf-8', decode='gbk'):
     if isinstance(data, bytes):
-        return hashlib.sha1(data).hexdigest()
-    return hashlib.sha1(data.encode(encode)).hexdigest()
+        return \
+            hashlib.sha1(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+    
+    return \
+        hashlib.sha1(data.encode(encode)).hexdigest()
 
 
-def sha224(data: str | bytes, *, encode='utf-8'):
+def sha224(data: str | bytes, *, encode='utf-8', decode='gbk'):
     if isinstance(data, bytes):
-        return hashlib.sha224(data).hexdigest()
-    return hashlib.sha224(data.encode(encode)).hexdigest()
+        return \
+            hashlib.sha224(
+                data.decode(decode).encode(encode)
+            ).hexdigest()
+    
+    return \
+        hashlib.sha224(data.encode(encode)).hexdigest()
