d095716695783325c6002a2d00bda03f9fac8caa
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-09-01 17:55:34 +0800

    Update __init__.py

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,7 +1,6 @@
 from ...utils import hash, batch_hash, disable_in_py3
 from ...utils.register import Register, functions, classes
-from ...utils.logger import \
-    logger, Logger
+from ...utils.logger import logger, Logger
 __all__ = [
     "hash", "Register", "batch_hash", "disable_in_py3",
 
