1f547a65e486bd8c945041479fc5195d91fc9ee3
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-12-23 16:27:19 +0800

    update

--- mcdr/command.py
+++ mcdr/command.py
@@ -3,19 +3,10 @@ MCDR 指令编辑包
 `from mymodule.mcdr.command import *`就行了
 """
 
-# Command Arguments
-from mcdreforged.command.builder.nodes.arguments import \
-    Number, Integer, Float, Text, QuotableText, \
-    GreedyText, Boolean, Enumeration
-from mcdreforged.command.builder.nodes.basic import \
-    AbstractNode, Literal, ArgumentNode
-
-# Plugin Server Interface
-from mcdreforged.api.types import ServerInterface, PluginServerInterface
-
 # Simple Command Builder
 from mcdreforged.api.command import SimpleCommandBuilder
-
+# Plugin Server Interface
+from mcdreforged.api.types import ServerInterface, PluginServerInterface
 # Exceptions
 from mcdreforged.command.builder.exception import \
     LiteralNotMatch, NumberOutOfRange, IllegalArgument, EmptyText, \
@@ -23,28 +14,72 @@ from mcdreforged.command.builder.exception import \
     RequirementNotMet, IllegalNodeOperation, CommandError, InvalidNumber, \
     InvalidInteger, InvalidFloat, UnclosedQuotedString, IllegalEscapesUsage, InvalidBoolean, \
     InvalidEnumeration, TextLengthOutOfRange, CommandErrorBase, AbstractOutOfRange
-
-# Plugin things
-from mcdreforged.plugin.meta.metadata import Metadata
-from mcdreforged.plugin.meta.version import Version, VersionRequirement
-
-# Info
-from mcdreforged.info_reactor.info import Info
-from mcdreforged.info_reactor.server_information import ServerInformation
-
-# Permission
-from mcdreforged.permission.permission_level import PermissionLevel
-
+# Command Arguments
+from mcdreforged.command.builder.nodes.arguments import \
+    Number, Integer, Float, Text, QuotableText, \
+    GreedyText, Boolean, Enumeration
+from mcdreforged.command.builder.nodes.basic import \
+    AbstractNode, Literal, ArgumentNode
 # Command sources
 from mcdreforged.command.command_source import \
     CommandSource, ConsoleCommandSource, PlayerCommandSource, \
     InfoCommandSource, PluginCommandSource
-
+# Info
+from mcdreforged.info_reactor.info import Info
+from mcdreforged.info_reactor.server_information import ServerInformation
+# Permission
+from mcdreforged.permission.permission_level import PermissionLevel
+# Plugin things
+from mcdreforged.plugin.meta.metadata import Metadata
+from mcdreforged.plugin.meta.version import Version, VersionRequirement
 # Loggers
 from mcdreforged.utils.logger import SyncStdoutStreamHandler, MCDReforgedLogger
 
+SCBInstance = builder = SimpleCommandBuilder()
 
-SCB = builder = SimpleCommandBuilder()
+
+class CommandNodes:
+    Number = Number
+    Integer = Integer
+    Float = Float
+    Text = Text
+    Enumeration = Enumeration
+    QuotableText = QuotableText
+    AbstractNode = AbstractNode
+    GreedyText = GreedyText
+    Literal = Literal
+    Boolean = Boolean
+    ArgumentNode = ArgumentNode
+
+
+class MCDReforgedExceptions:
+    LiteralNotMatch = LiteralNotMatch
+    UnclosedQuotedString = UnclosedQuotedString
+    NumberOutOfRange = NumberOutOfRange
+    IllegalEscapesUsage = IllegalEscapesUsage
+    IllegalArgument = IllegalArgument
+    InvalidBoolean = InvalidBoolean
+    EmptyText = EmptyText
+    InvalidEnumeration = InvalidEnumeration
+    UnknownCommand = UnknownCommand
+    TextLengthOutOfRange = TextLengthOutOfRange
+    UnknownArgument = UnknownArgument
+    CommandErrorBase = CommandErrorBase
+    CommandSyntaxError = CommandSyntaxError
+    AbstractOutOfRange = AbstractOutOfRange
+    UnknownRootArgument = UnknownRootArgument
+    InvalidNumber = InvalidNumber
+    RequirementNotMet = RequirementNotMet
+    InvalidInteger = InvalidInteger
+    IllegalNodeOperation = IllegalNodeOperation
+    InvalidFloat = InvalidFloat
+    CommandError = CommandError
+
+
+class PluginUtils:
+    PluginServerInterface = PluginServerInterface
+    Exceptions = MCDReforgedExceptions
+    Nodes = CommandNodes
 
 
 __all__ = [
@@ -68,7 +103,7 @@ __all__ = [
     #     Command Builder
     # -------------------------
 
-    'SimpleCommandBuilder', 'SCB', 'builder',
+    'SimpleCommandBuilder', 'SCBInstance', 'builder',
 
     # --------------------------
     #   MCDReforged Exceptions
@@ -119,4 +154,9 @@ __all__ = [
     # ------------------------
     'SyncStdoutStreamHandler',
     'MCDReforgedLogger',
+
+    # --------------------
+    #      some tools
+    # --------------------
+    'CommandNodes', 'MCDReforgedExceptions',
 ]
--- utils/disable_in_py3.py
+++ utils/disable_in_py3.py
@@ -2,7 +2,9 @@ __all__ = [
     "cmp",
 ]
 
+from numbers import Real
+from typing import Literal
 
-def cmp(x, y):
+
+def cmp(x: Real, y: Real) -> Literal[-1, 0, 1]:
     return (x > y) - (x < y)
-
