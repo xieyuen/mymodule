b1ddef8a150750f56394581d2353573808fe1da5
xieyuen <107105640+xieyuen@users.noreply.github.com>
2024-04-13 23:14:55 +0800

    重构 utils

--- mymodule/utils/__init__.py
+++ mymodule/utils/__init__.py
@@ -1,2 +1,8 @@
 def bind(fn, *bind_args, **bind_kwargs):
     return lambda *args, **kwargs: fn(*bind_args, *args, **bind_kwargs, **kwargs)
+
+
+def getOptions(opt: dict, default: dict) -> dict:
+    result = default.copy()
+    result.update(opt)
+    return result
